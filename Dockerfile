544e8f3f46da080872afde3ec2e8fc7bc12599b0
Marcus Lankenau <marcus.lankenau@2trde.com>
2017-08-04 09:46:22 +0200

    fix the ES_JAVA_OPTS not taken issue

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
-FROM docker.elastic.co/elasticsearch/elasticsearch:5.5.1
+FROM elasticsearch:5.5.1
+ADD elasticsearch.yml /usr/share/elasticsearch/config/
 USER root
-CMD chmod 777 /usr/share/elasticsearch/data; /usr/sbin/runuser -l elasticsearch -c bin/es-docker
+RUN chown elasticsearch:elasticsearch config/elasticsearch.yml
+#CMD chmod 777 /usr/share/elasticsearch/data; ES_JAVA_OPTS=$ES_JAVA_OPTS /usr/sbin/runuser -p -l elasticsearch -c bin/es-docker
+CMD chmod 777 /usr/share/elasticsearch/data; su elasticsearch -c bin/es-docker
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-docker build -t elasticsearch:5.5.1 . || exit
-docker tag elasticsearch:5.5.1 mlankenau/elasticsearch:5.5.1 || exit
-docker tag elasticsearch:5.5.1 mlankenau/elasticsearch:latest || exit
-docker push mlankenau/elasticsearch:5.5.1 || exit
+export tag=5.5.1
+docker build -t elasticsearch:$tag . || exit
+docker tag elasticsearch:$tag mlankenau/elasticsearch:$tag || exit
+docker tag elasticsearch:$tag mlankenau/elasticsearch:latest || exit
+docker push mlankenau/elasticsearch:$tag || exit
 docker push mlankenau/elasticsearch:latest || exit
